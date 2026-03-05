99e33f05fc70de13aa6ea8184d895412415f2889
Sonu64 <sonusantu64@gmail.com>
2026-03-05 15:57:45 +0530

    Initial Spring Boot Setup. Started week1.

---
+++ Week1/Module_01_Intro/Module_01_Intro/src/main/java/com/sonusantu64/gmail/Module_01_Intro/Module01IntroApplication.java
@@ -0,0 +1,13 @@
+package com.sonusantu64.gmail.Module_01_Intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module01IntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module01IntroApplication.class, args);
+	}
+
+}
---
+++ Week1/Module_01_Intro/Module_01_Intro/src/test/java/com/sonusantu64/gmail/Module_01_Intro/Module01IntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonusantu64.gmail.Module_01_Intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module01IntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
