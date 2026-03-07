4a03dbd7ea6e4f76ccc110eb86860b5d63edbf2b
Sonu64 <sonusantu64@gmail.com>
2026-03-07 09:04:36 +0530

    Observed that Singleton Scope for Beans is the best way and used in most of the cases. Checked via hashCode.

--- Week1/Module_01_Intro/Module_01_Intro/src/main/java/com/sonusantu64/gmail/Module_01_Intro/Module01IntroApplication.java
+++ Week1/Module_01_Intro/Module_01_Intro/src/main/java/com/sonusantu64/gmail/Module_01_Intro/Module01IntroApplication.java
@@ -10,12 +10,21 @@ public class Module01IntroApplication implements CommandLineRunner {
 	@Autowired
 	PaymentService paymentServiceObj;
 
+	@Autowired
+	PaymentService paymentServiceObj2;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Module01IntroApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		System.out.println(paymentServiceObj.hashCode());
+		System.out.println(paymentServiceObj2.hashCode());
+		//  Both have Same HashCode -> Pointing to same object in memory -> Singleton Scoped Beans are most common
+		// and used for >99% of applications. Other scopes are considered 'Anti-Design' Pattern and not recommended
+		// to use.
 		paymentServiceObj.pay();
+		paymentServiceObj2.pay();
 	}
 }
